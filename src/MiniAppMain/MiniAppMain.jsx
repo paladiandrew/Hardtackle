4a0486f0436426097f65e1f4f7cc24478d293989
paladiandrew <paladi.andrew@yandex.ru>
2025-01-25 17:54:07 +0300

    New Web

--- src/MiniAppMain/MiniAppMain.jsx
+++ src/MiniAppMain/MiniAppMain.jsx
@@ -1,17 +1,16 @@
 // src/MainScreen/MainScreen.jsx
 import React from "react";
 import { Link } from "react-router-dom";
-import './MiniAppMain.css';
+import './MiniAppMain.css'; // Импортируем стили
 
 export default function MiniAppMain() {
     return (
         <div className="container">
-            <h1>Мини апп</h1>
             <Link to="/registration">
-                <button>Регистрация</button>
+                <button className="button">Регистрация</button>
             </Link>
             <Link to="/past-results">
-                <button>Результаты прошлых этапов</button>
+                <button className="button">Результаты прошлых этапов</button>
             </Link>
         </div>
     );
