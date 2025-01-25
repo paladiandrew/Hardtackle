9017781ec28d9eac72c6ac619cbfb9a101698ace
paladiandrew <paladi.andrew@yandex.ru>
2025-01-25 17:33:07 +0300

    New Web

--- src/MiniAppMain/MiniAppMain.jsx
+++ src/MiniAppMain/MiniAppMain.jsx
@@ -1,10 +1,11 @@
 // src/MainScreen/MainScreen.jsx
 import React from "react";
 import { Link } from "react-router-dom";
+import './MiniAppMain.css';
 
 export default function MiniAppMain() {
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+        <div className="container">
             <h1>Мини апп</h1>
             <Link to="/registration">
                 <button>Регистрация</button>
